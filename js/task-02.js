622b6d27073aba42ed74700bd07668d35431428f
AlexSol1702 <105703672+AlexSol1702@users.noreply.github.com>
2022-09-21 18:14:59 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,21 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const elems = [];
+
+const ingredientsRef = items => {
+  items.map((item) => {
+    const ingredientRef = document.createElement("li");
+    ingredientRef.textContent = `${item}`;
+    ingredientRef.classList.add("item");
+
+    return elems.push(ingredientRef); 
+  })
+    const ingredientsList = document.querySelector("#ingredients");
+    ingredientsList.append(...elems);
+};
+  
+ingredientsRef(ingredients);
+
+
