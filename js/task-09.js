c551f62600b604175c75feae3935b70c0aa77f80
AlexSol1702 <105703672+AlexSol1702@users.noreply.github.com>
2022-09-26 10:32:22 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const refs = {
+  text: document.querySelector('.color'),
+  btn: document.querySelector('.change-color'),
+  body: document.querySelector('body'),
+};
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+  refs.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  refs.text.textContent = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+ 
+refs.btn.addEventListener('click', getRandomHexColor);
+
+
