1f7fb56bf1476a6e03b9d28709e630be9912be9f
AlexSol1702 <105703672+AlexSol1702@users.noreply.github.com>
2022-09-26 09:36:44 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    const enteredData = []
+
+    const { elements: { email, password } } = e.currentTarget;
+
+    if (email.value === ''||password.value==='') {
+       return console.log('Alert! Please in all fields.');
+    } else {
+         enteredData.push(`email: ${email.value}, password: ${password.value}`);
+    };
+    console.log(enteredData);
+    e.currentTarget.reset();
+}
